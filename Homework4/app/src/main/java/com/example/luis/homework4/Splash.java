62902eb04f2eab9fcd608758dc0c33efffaa07b9
Francisco Robleto <frobleto323@gmail.com>
2016-04-24 17:05:08 -0500

    current work for hw4

---
+++ Homework4/app/src/main/java/com/example/luis/homework4/Splash.java
@@ -0,0 +1,16 @@
+package com.example.luis.homework4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Luis on 4/24/2016.
+ */
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.longhorn_screen);
+    }
+}
